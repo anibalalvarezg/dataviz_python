9e9a1999bb47bc43b63c209081091a84a3d4fe81
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-06-06 18:36:24 -0400

    Dates - time - part 2

--- dates_time.py
+++ dates_time.py
@@ -12,3 +12,17 @@ def dumps(obj):
 
 now_str = dumps({'time': datetime.datetime.now()})
 print(now_str)
+
+from datetime import datetime
+time_str = '2012/01/01 12:32:11'
+dt = datetime.strptime(time_str, '%Y/%m/%d %H:%M:%S')
+print(dt)
+
+# To convert date fields of a known format into datetimes:
+# dt = datetime.strptime('1/2/2012 12:32:11', '%Y/%m/%d %H:%M:%S')
+# for d in data:
+#     try:
+#         d['date'] = datetime.strptime(d['date'],\
+#         '%Y/%m/%d %H:%M:%S')
+#     except ValueError:
+#         print('Oops! - invalid date for ' + repr(d))
